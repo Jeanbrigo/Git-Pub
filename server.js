ac35b16da887ed3ba56e5039a556a83799c3ca23
Jean Brigonnet <jeanbrigo@gmail.com>
2022-11-06 21:43:47 -0600

    set up index view to show drinks data

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const drinks = require('./models/drinks');
-// const drinksIndex = require('./views/drinks_index.ejs');
 
 app.get('/', (req,res)=>{
     res.send('Welcome to the Gitpub App!');
 })
 
 app.get('/drinks', (req,res)=>{
-    res.render('drinks_index.ejs');
+    res.render('drinks_index.ejs', { drinks });
 })
 
 app.listen(port, () => {
