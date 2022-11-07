3c0f161fe15290ee6a62d3326a01f737b4bac7b4
Jean Brigonnet <jeanbrigo@gmail.com>
2022-11-06 21:45:06 -0600

    set up show route

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/drinks', (req,res)=>{
     res.render('drinks_index.ejs', { drinks });
 })
 
+app.get('/drinks/:id', (req,res)=>{
+    res.send(req.params.id)
+})
+
 app.listen(port, () => {
 	console.log(`Express is listening on port ${port}...`);
 });
