05c81a8ec13286313215c5fb76082a5b180d3a8d
Jean Brigonnet <jeanbrigo@gmail.com>
2022-11-06 22:58:36 -0600

    added food to menu and show pages

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const drinks = require('./models/drinks');
+const food = require('./models/food');
 
 app.get('/', (req,res)=>{
     res.send('Welcome to the Gitpub App!');
 })
 
 app.get('/drinks', (req,res)=>{
-    res.render('drinks_index.ejs', { allDrinks:drinks });
+    res.render('drinks_index.ejs', { allDrinks:drinks, allFood:food });
 })
 
 app.get('/drinks/:id', (req,res)=>{
     res.render('drinks_show.ejs', { drink:drinks[req.params.id] })
+})
+
+app.get('/food/:id', (req,res)=>{
+    res.render('food_show.ejs', { feed:food[req.params.id] })
 })
 
 app.listen(port, () => {
